60c3d0e97b076d88a0514a0b77f43a72eea9733d
Rimsha Aslam <rimsha.aslam.230@gmail.com>
2024-06-07 20:25:51 +0500

    push

---
+++ app.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+import chalk from "chalk";
+const res = await inquirer.prompt([
+    {
+        name: "userinput",
+        type: "number",
+        message: "Please enter the amount of second",
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter valid number";
+            }
+            else if (input > 60) {
+                return "Seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+let input = res.userinput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((() => {
+        const currTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currTime);
+        if (timeDiff <= 0) {
+            console.log((chalk.bold.italic.underline.yellow("Timer has expired")));
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(input);
